a7f1bdce3c83b12b1c251a429460d94f291d4170
Tehpuccupharum <mochammad.yusuf@raharja.info>
2024-06-23 15:07:17 +0700

    fix

--- event-service/internal/domain/model/category.go
+++ event-service/internal/domain/model/category.go
@@ -1,6 +1,6 @@
 package model
 
 type Category struct {
-	CategoryID   int    `json:"category_id" db:"category_id"`
+	CategoryID int    `json:"category_id" db:"category_id"`
 	CategoryName string `json:"category_name" db:"category_name"`
 }
